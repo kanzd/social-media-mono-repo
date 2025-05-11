f328842358eab2d91363891c6b1ab6bb84ce601a
Kanishk.Dixit <Z00DKXM>
2025-05-11 06:45:12 +0530

    stage deployment

--- Social-Media-Web-App-Mern-Stack-/Server/index.js
+++ Social-Media-Web-App-Mern-Stack-/Server/index.js
@@ -24,7 +24,7 @@ app.use(cors());
 dotenv.config();
 
 
-const connection = 'mongodb+srv://algotarian:8r4pk8qW9LjE3fw7@cluster0.5wzvoid.mongodb.net/'
+const connection = 'mongodb://mongo:27017/'
 mongoose.connect
     (connection, { useNewUrlParser: true, useUnifiedTopology: true,dbName: "hik8", }
     ).then(() =>
