ffb772daba9501976ecf21123193c948c2a0d939
Kanishk.Dixit <Z00DKXM>
2025-05-11 14:22:26 +0530

    stage deployment

--- Social-Media-Web-App-Mern-Stack-/client/src/api/CommentRequest.js
+++ Social-Media-Web-App-Mern-Stack-/client/src/api/CommentRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://stage.hik8.com:4000' });
+const API = axios.create({ baseURL: 'http://stage.hik8.com:4000' });
 
 export const getPostComments = (id) => API.get(`/comment/${id}/post_comments`);
 export const likeComment = (id, likedBy ) => API.put(`/comment/${id}/like_dislike`, { likedBy: likedBy });
--- Social-Media-Web-App-Mern-Stack-/client/src/api/UploadRequest.js
+++ Social-Media-Web-App-Mern-Stack-/client/src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://stage.hik8.com:4000' });
+const API = axios.create({ baseURL: 'http://stage.hik8.com:4000' });
 
 export const uploadImage = (data) => API.post('/upload/', data);
 export const uploadPost = (data) => API.post('/post', data);
